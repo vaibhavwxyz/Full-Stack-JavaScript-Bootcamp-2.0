a56581af004584ea1c4fa4be2f72ba19a1760ff6
vaibhavwxyz <vaibhawxyz@gmail.com>
2022-12-14 15:35:48 +0530

    all done

---
+++ Practice Code/WEEK06- DAY01 - 10th DEC- SATURDAY/script.js
@@ -0,0 +1,85 @@
+// Number values
+let x = 5;
+let y = 9;
+
+let ans = x + y;
+
+console.log(ans);
+
+
+// String values
+let name = 'vaibhav';
+let surname = 'shinde';
+
+console.log('my first name is', name);
+console.log('my last name is', surname);
+
+// Boolean values
+let a = true;   // 1
+let b = false;  // 0
+
+// null and undefined
+// empty values
+
+
+// Array and Object
+let array = [1, 2, 'vaibhav', 'shinde', 5, 6];
+
+const myDuke = {
+    color: "orange",
+    wheels: 2,
+    fuel: "petrol"
+};
+
+
+// Template literal
+console.log(`my full name is ${name} ${surname}`);
+
+
+// Operators
+
+// 1. arthimatic operator
+let z = x + y;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// 2. comparison operator
+let abc = 10;
+let xyz = '10';
+
+console.log(abc > xyz);
+console.log(abc <= xyz);
+console.log(abc >= xyz);
+// == OR ==
+console.log(abc===xyz);
+
+
+// Conditions
+let jerseyNumber = 18;
+
+if (jerseyNumber == 14) {
+    console.log('Hello Rickey Ponting');
+} else if (jerseyNumber == 17) {
+    console.log('Hello AB de Villiers');
+} else if (jerseyNumber == 18) {
+    console.log('Hello Virat Kohli');
+} else {
+    console.log('Sorry! Jersey number not found');
+}
+
+/*                          <- HOMEWORK ->
+
+History
+Basic Pillar of an programming Language
+Values
+Data Types
+Difference between Primitive and non primitive Datatypes
+Console.log => How to print something on terminal
+Decalring Variable
+Tempalte Literal
+
+*/
