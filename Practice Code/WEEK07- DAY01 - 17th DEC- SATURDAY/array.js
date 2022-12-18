0648aa5ed2ec447052a1a5d55fc8915665d83489
vaibhavwxyz <vaibhawxyz@gmail.com>
2022-12-18 17:52:56 +0530

    week07day01

--- Practice Code/WEEK07- DAY01 - 17th DEC- SATURDAY/array.js
+++ Practice Code/WEEK07- DAY01 - 17th DEC- SATURDAY/array.js
@@ -1,8 +1,23 @@
-// let names = ["vaibhav", "sai", "prashant", 24, 69, "sujit", "vinayak"];
-// names.pop("aniket");
-// console.log(names);
+// ARRAY METHODS/FUNCTIONS
 
-let names = ["vaibhav", "sai", "prashant", 24, 69, "sujit", "vinayak"];
-let firstElement = names.shift();
-console.log(names);
-console.log(firstElement);
+
+// 1. push()
+let names1 = ["vaibhav", "sai", "prashant", 24, 69, "sujit", "vinayak"];
+names1.push("aniket");
+console.log(names1);
+
+// 2. pop()
+let names2 = ["vaibhav", "sai", "prashant", 24, 69, "sujit", "vinayak"];
+names2.pop("aniket");
+console.log(names2);
+
+
+
+// 6. slice
+let city1 = ["pune", "mumbai", "aurangabad", "kolhapur", "nashik"];
+console.log(city1.slice(1, 4));
+
+// 7. splice
+let city2 = ["pune", "mumbai", "aurangabad", "kolhapur", "nashik"];
+city2.splice(2, 1, "delhi");
+console.log(city2);
