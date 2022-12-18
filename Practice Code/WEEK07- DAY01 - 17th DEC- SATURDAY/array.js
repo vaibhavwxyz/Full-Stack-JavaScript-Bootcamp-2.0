f218c4bf32fc4569f03ce685600753db5787cd13
vaibhavwxyz <vaibhawxyz@gmail.com>
2022-12-18 17:33:54 +0530

    week07day02

---
+++ Practice Code/WEEK07- DAY01 - 17th DEC- SATURDAY/array.js
@@ -0,0 +1,8 @@
+// let names = ["vaibhav", "sai", "prashant", 24, 69, "sujit", "vinayak"];
+// names.pop("aniket");
+// console.log(names);
+
+let names = ["vaibhav", "sai", "prashant", 24, 69, "sujit", "vinayak"];
+let firstElement = names.shift();
+console.log(names);
+console.log(firstElement);
