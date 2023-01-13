6cdba027afc7265a3cebb197e73bc6975a8a716c
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-01-13 13:24:00 +0530

    played with objects

--- Practice Code/WEEK10- DAY01 - 7th JAN- SATURDAY/script.js
+++ Practice Code/WEEK10- DAY01 - 7th JAN- SATURDAY/script.js
@@ -1,4 +1,4 @@
-// console.log(this);
+console.log(this);
 
 const object = {
     rocket: '🚀',
@@ -6,7 +6,7 @@ const object = {
 }
 
 for (k in object){
-    // console.log(k);
+    console.log(k);
 }
 
 // string, number, bigint, boolean, null, undefine, symbol
@@ -23,7 +23,7 @@ vehicle.fuel = 200
 vehicle['year'] = 2000
 
 for (k in vehicle){
-    // console.log(k);
+    console.log(k);
 }
 
 // PART 2
@@ -31,7 +31,7 @@ const books = new Object() //constructor
 books.comic = 'marvel'
 books.price = '299' // we can't define books.1stPrice
 
-// console.log(books);
+console.log(books);
 
 
 // PART 3
@@ -41,27 +41,27 @@ const powers = {
 }
 
 const hero = Object.create(powers)
-// console.log(hero);
-// console.log(hero.cordinate);
-// console.log(Object.getPrototypeOf(hero));
+console.log(hero);
+console.log(hero.cordinate);
+console.log(Object.getPrototypeOf(hero));
 
 
 // PART 4
 const fruit = Object.create({})
 fruit.red = "apple"
-// console.log(fruit);
+console.log(fruit);
 Object.defineProperty(fruit, 'yellow', {
     // icon: 'banana'
     get: () => 'banana',
     enumerable: true
 })
 
-// console.log(fruit.yellow);
-// console.log(Object.getPrototypeOf(fruit));
-// console.log(fruit);
+console.log(fruit.yellow);
+console.log(Object.getPrototypeOf(fruit));
+console.log(fruit);
 
 for(k in fruit){
-    // console.log("value is: ", k);
+    console.log("value is: ", k);
 }
 
 
@@ -74,13 +74,13 @@ const objects = {
         this.pen += 'reynold';
         console.log("this is inside the printPen: ", this);
     },
-    // printNotebook: () => {
-    //     this.notebook += 'classmate'; //this will not updated the value because of arrow function does't know 'this' concept.
-    //     console.log("this is inside the printNotebook function: ", this);
-    // }
+    printNotebook: () => {
+        this.notebook += 'classmate'; //this will not updated the value because of arrow function does't know 'this' concept.
+        console.log("this is inside the printNotebook function: ", this);
+    }
 }
 
-// console.log("this is printPen: ", objects.printPen());
-// console.log("this is printNotebook: ", objects.printNotebook());
+console.log("this is printPen: ", objects.printPen());
+console.log("this is printNotebook: ", objects.printNotebook());
 
 console.log(objects.printPen().printPen().printPen());
