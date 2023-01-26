28c9d23d473f52ba6d7d1728a302bcda762bc51a
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-01-26 14:57:25 +0530

    final commit

---
+++ Projects and Assignments/03- JavaScript/Assigment/Question- 01/variable.js
@@ -0,0 +1,23 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// We can declare variable using the keywords let, var and const.
+
+
+// Undefined
+let value = undefined;
+console.log(value, `Type of this variable is ${typeof value}`);
+
+// Null
+let digit = null;
+console.log(digit, `Type of this variable is ${typeof digit}`);
+
+// String
+let fName = "Vaibhav";
+console.log(fName, `Type of this variable is ${typeof fName}`);
+
+// Boolean
+let x = true;
+let y = false;
+
+console.log(x, `Type of this variable is ${typeof x}`);
+console.log(y, `Type of this variable is ${typeof y}`);
