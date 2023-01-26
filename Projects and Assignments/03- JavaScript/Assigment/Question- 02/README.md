bd7a2a8cdc6b538c28abfec64a8716d0595b8848
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-01-26 15:23:18 +0530

    final commit

---
+++ Projects and Assignments/03- JavaScript/Assigment/Question- 02/README.md
@@ -0,0 +1,3 @@
+# <img height="24px" src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png"> Question:
+
+- ## Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
---
+++ Projects and Assignments/03- JavaScript/Assigment/Question- 02/script.js
@@ -0,0 +1,12 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interpolation method.
+
+/* NOTE: Interpolation is a way to insert a variable or expression into a string, typically using template literals in JavaScript.
+This is done by enclosing the variable or expression in curly braces {} and preceding it with a dollar sign */
+
+let fName = "Vaibhav";
+let lName = "Shinde";
+let maritalStatus = "Unmarried";
+let country = "India";
+let age = 24;
+
+console.log(`My name is ${fName} ${lName}. I'm from ${country}. I'm ${age} years old and I'm ${maritalStatus}`);
