2e519f6e20b6fa535430a0a8d37b830602c1d0d4
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-08 19:41:12 +0530

    practice

---
+++ Code Practice/WEEK14- DAY02 - 05th FEB- SUNDAY/script.js
@@ -0,0 +1,18 @@
+// console.log(foo);
+// var foo = "bar";
+
+// price();
+// function price() {
+//   let soap = 29;
+//   console.log(soap);
+//   discount();
+//   function discount() {
+//     soap = 27;
+//     let finalPrice = 21;
+//     console.log(soap);
+//     console.log(finalPrice);
+//   }
+// }
+
+let foo = "bar";
+console.log(typeof foo);
