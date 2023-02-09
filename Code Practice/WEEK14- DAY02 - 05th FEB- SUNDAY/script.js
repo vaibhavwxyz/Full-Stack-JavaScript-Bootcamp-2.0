aded60a6c4e2a1a6315637cbccfe3765e8863a2d
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-09 18:54:48 +0530

    practice

--- Code Practice/WEEK14- DAY02 - 05th FEB- SUNDAY/script.js
+++ Code Practice/WEEK14- DAY02 - 05th FEB- SUNDAY/script.js
@@ -16,20 +16,53 @@
 
 // let foo = "bar";
 // console.log(typeof foo
-a();
-function a() {
-	let x = 10;
-	// console.log(x);
-	// console.log(y);
-	b();
-	function b() {
-		let y = 20;
-		console.log(x);
-		c();
-		function c() {
-			let z = 30;
-			console.log(x);
-			console.log(y);
-		}
-	}
+// a();
+// function a() {
+// 	let x = 10;
+// 	// console.log(x);
+// 	// console.log(y);
+// 	b();
+// 	function b() {
+// 		let y = 20;
+// 		console.log(x);
+// 		c();
+// 		function c() {
+// 			let z = 30;
+// 			console.log(x);
+// 			console.log(y);
+// 		}
+// 	}
+// }
+
+{
+	// block, we group together multiple statements in blocks
+	// var a = 10;
+	// console.log(a);
 }
+
+// if (true) console.log(a); // valid
+
+// // what all var an funcitons that access inside the block
+
+// {
+// 	var aa = 10;
+// 	let bb = 20;
+// 	const cc = 30;
+// 	console.log(aa);
+// 	console.log(bb);
+// 	console.log(cc);
+// }
+// console.log(aa);
+// console.log(bb);
+// console.log(cc);
+
+var a = 100;
+{
+	var a = 10;
+	let b = 20;
+	const c = 30;
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+console.log(a);
