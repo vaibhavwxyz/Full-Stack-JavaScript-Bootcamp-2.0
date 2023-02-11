833c7f224ce22152d6fe6f52dcb88ed90c378d12
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-11 16:14:50 +0530

    dom

---
+++ Code Practice/WEEK13- DAY01 - 28th JAN- SATURDAY/script.js
@@ -0,0 +1,21 @@
+//! Previous Day Warm-Up
+// console.log(btn);
+
+// let h2 = document.querySelectorAll("h2");
+// const btn = document.querySelector(".btn");
+
+// for (let i = 0; i < h2.length; i++) {
+// 	h2[i].style.color = "red";
+// }
+
+// btn.addEventListener("click", (e) => {
+// 	for (let i = 0; i < h2.length; i++) {
+// 		h2[i].style.color = "red";
+// 	}
+// });
+const h1 = document.querySelector("h1");
+const li = document.createElement("li");
+// console.log(li);
+li.innerText = "x-men";
+li.style.padding = "24px";
+h1.append(li);
