e1715eb38f87b601397831caa68e188261d25125
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-14 19:44:01 +0530

    practice

--- Code Practice/WEEK13- DAY01 - 28th JAN- SATURDAY/script.js
+++ Code Practice/WEEK13- DAY01 - 28th JAN- SATURDAY/script.js
@@ -58,8 +58,10 @@ const hiddenContent = document.querySelector(".hidden-content");
 function revealContent() {
 	if (hiddenContent.classList.contains("reveal-btn")) {
 		hiddenContent.classList.remove("reveal-btn");
+		// console.log(hiddenContent.classList);
 	} else {
 		hiddenContent.classList.add("reveal-btn");
+		// console.log(hiddenContent.classList);
 	}
 }
 
