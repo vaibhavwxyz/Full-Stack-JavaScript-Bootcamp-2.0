90c7e986cbccfc210f717cfa600a2c927aefbc58
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-22 15:01:21 +0530

    react

---
+++ Code Practice/WEEK15- DAY02 - 12th FEB- SUNDAY/script.js
@@ -0,0 +1,12 @@
+// const name = "vaibhav"; // JavaScript
+// <h1>Vaibhav Shinde</h1>; // HTML
+
+// const jsxName = <h1>Vaibhav Shinde</h1>; // JSX
+// console.log(jsxName);
+
+// const header =
+//   <header>
+//     <h1>Hello</h1>
+//     <p>World</p>
+//   </header>
+//
