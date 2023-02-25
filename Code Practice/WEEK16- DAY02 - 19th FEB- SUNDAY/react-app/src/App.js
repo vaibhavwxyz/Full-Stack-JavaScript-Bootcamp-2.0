5e72bcec2da6ba884a2c957a9d544a3ad1659843
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-25 14:55:56 +0530

    react

---
+++ Code Practice/WEEK16- DAY02 - 19th FEB- SUNDAY/react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./card";
+
+function App() {
+  return (
+    <>
+      <div>App</div>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ Code Practice/WEEK16- DAY02 - 19th FEB- SUNDAY/react-app/src/card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card() {
+  return (
+    <>
+      <div>This is a card</div>
+    </>
+  );
+}
+
+export default Card;
