58c1ea0107102b53535a1556aad290fc02312587
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-25 18:52:39 +0530

    react

--- Code Practice/WEEK16- DAY02 - 19th FEB- SUNDAY/react-app/src/App.js
+++ Code Practice/WEEK16- DAY02 - 19th FEB- SUNDAY/react-app/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./card";
+import Axios from "axios";
 
 function App() {
+  const [details, setDetails] = useState({});
+  const fetchDetails = async () => {
+    const { data } = await Axios.get("https://randomuser.me/api");
+    console.log("RESPONSE", data);
+    const details = data.results[0];
+    setDetails(details);
+  };
+  let list = ["vaibhav", "sai", "prashant"];
+
   return (
     <>
       <div>App</div>
-      <Card />
+      <Card myname="vaibhav" list={list} details={details} />
+      <button onClick={fetchDetails}>get details</button>
     </>
   );
 }
--- Code Practice/WEEK16- DAY02 - 19th FEB- SUNDAY/react-app/src/card.jsx
+++ Code Practice/WEEK16- DAY02 - 19th FEB- SUNDAY/react-app/src/card.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-function Card() {
+function Card({ myname, list, details }) {
+  // console.log(props);
+
   return (
     <>
       <div>This is a card</div>
+      <div>myname</div>
+      <h1>{details.gender}</h1>
+      <h1>{details.name?.first}</h1>
+      <ul>
+        {list.map((l) => (
+          <li key={l}>{l}</li>
+        ))}
+      </ul>
     </>
   );
 }
