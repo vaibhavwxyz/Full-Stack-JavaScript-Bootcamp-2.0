81b96cef13ff61d41a656e1aedcc06264a3c85a0
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-04 19:01:32 +0530

    todo-app

--- Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
+++ Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import CustomForm from "./components/CustomForm";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   return (
     <div className="container">
       <header>
--- Projects and Assignments/04- React/01- TODO App/todo-app/src/components/CustomForm.jsx
+++ Projects and Assignments/04- React/01- TODO App/todo-app/src/components/CustomForm.jsx
@@ -1,4 +1,8 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+
 const CustomForm = () => {
+  const [task, setTask] = useState("");
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log(e);
@@ -6,9 +10,26 @@ const CustomForm = () => {
 
   return (
     <form className="todo" onSubmit={handleFormSubmit}>
+      <p>{task}</p>
       <div className="wrapper">
-        <h1>hello</h1>
+        <input
+          type="text"
+          id="task"
+          className="input"
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
+          autoFocus
+          required
+          maxLength={60}
+          placeholder="Enter Task"
+        />
+        <label htmlFor="task" className="label">
+          Enter Task
+        </label>
       </div>
+      <button className="btn" aria-label="Add Task" type="submit">
+        <PlusIcon className="h-6 w-6 text-blue-500" />
+      </button>
     </form>
   );
 };
