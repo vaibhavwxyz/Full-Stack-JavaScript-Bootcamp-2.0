2caa480944f104c6f15a578198b5e0d886896d15
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-05 17:36:29 +0530

    todo-app

--- Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
+++ Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import CustomForm from "./components/CustomForm";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const addTask = (task) => {
+    console.log(task);
+  };
   return (
-    <div className="container">
-      <header>
-        <h1>My Task List</h1>
-      </header>
-      <CustomForm />
-    </div>
+    <>
+      <CustomForm addTask={addTask} />
+    </>
   );
 }
 
--- Projects and Assignments/04- React/01- TODO App/todo-app/src/components/CustomForm.jsx
+++ Projects and Assignments/04- React/01- TODO App/todo-app/src/components/CustomForm.jsx
@@ -1,35 +1,40 @@
 import { PlusIcon } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
 
-const CustomForm = () => {
+const CustomForm = ({ addTask }) => {
   const [task, setTask] = useState("");
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now(),
+    });
+    setTask("");
   };
 
   return (
-    <form className="todo" onSubmit={handleFormSubmit}>
-      <p>{task}</p>
-      <div className="wrapper">
-        <input
-          type="text"
-          id="task"
-          className="input"
-          value={task}
-          onInput={(e) => setTask(e.target.value)}
-          autoFocus
-          required
-          maxLength={60}
-          placeholder="Enter Task"
-        />
-        <label htmlFor="task" className="label">
-          Enter Task
-        </label>
+    <form className="lg:p-20" onSubmit={handleFormSubmit}>
+      <div className="flex flex-col gap-y-5">
+        <h1 className="text-center text-2xl font-bold">My Task List</h1>
+        <div className="flex gap-x-2 justify-center items-center">
+          <input
+            type="text"
+            id="task"
+            className="px-2 border-[2px] outline-none drop-shadow-xl rounded-full max-w-xs placeholder:text-blue-500"
+            value={task}
+            onInput={(e) => setTask(e.target.value)}
+            autoFocus
+            required
+            maxLength={60}
+            placeholder="Enter Task"
+          />
+          <button className="h-10 w-10" aria-label="Add Task" type="submit">
+            <PlusIcon className="h-6 w-6 text-blue-500" />
+          </button>
+        </div>
       </div>
-      <button className="btn" aria-label="Add Task" type="submit">
-        <PlusIcon className="h-6 w-6 text-blue-500" />
-      </button>
     </form>
   );
 };
