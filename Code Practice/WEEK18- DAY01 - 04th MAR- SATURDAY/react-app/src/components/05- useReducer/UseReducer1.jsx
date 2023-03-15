a269e4a5d07260d94f7e27a2fe7ad10077eb8e49
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-15 19:15:03 +0530

    useReducer

--- Code Practice/WEEK18- DAY01 - 04th MAR- SATURDAY/react-app/src/components/05- useReducer/UseReducer1.jsx
+++ Code Practice/WEEK18- DAY01 - 04th MAR- SATURDAY/react-app/src/components/05- useReducer/UseReducer1.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  return { count: state.count + 1 };
+};
 
 const UseReducer1 = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   function increment() {
-    setCount((prev) => prev + 1);
+    dispatch();
   }
 
-  function decrement() {
-    setCount((prev) => prev - 1);
-  }
+  function decrement() {}
   return (
     <>
+      <h2>Count: {state.count}</h2>
       <button onClick={increment}>increment</button>
-      {count}
       <button onClick={decrement}>decrement</button>
     </>
   );
