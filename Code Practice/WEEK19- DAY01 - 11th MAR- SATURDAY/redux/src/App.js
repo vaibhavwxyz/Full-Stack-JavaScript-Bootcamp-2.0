dfb7ca7be8394752970cafbcc31aba76193c487f
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-18 20:53:45 +0530

    redux

--- Code Practice/WEEK19- DAY01 - 11th MAR- SATURDAY/redux/src/App.js
+++ Code Practice/WEEK19- DAY01 - 11th MAR- SATURDAY/redux/src/App.js
@@ -22,7 +22,10 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        <button
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          onClick={() => dispatch(incrementByValue(+input))}
+        >
           Increment by Value
         </button>
       </div>
