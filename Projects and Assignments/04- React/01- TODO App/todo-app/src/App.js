144ca1860cb888c0f57173c75a28f7dbe13f4151
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-20 17:59:26 +0530

    todo-app

--- Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
+++ Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
@@ -1,16 +1,62 @@
-import React, { useState } from "react";
-import CustomForm from "./components/CustomForm";
-import TaskList from "./components/TaskList";
+import { useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const addTask = (task) => {
-    setTasks((prevState) => [...prevState, task]);
-  };
+  const [task, setTask] = useState("");
+  const [listData, setListData] = useState([]);
+
+  function addTask() {
+    // setListData(...listData, task);
+    // console.log(listData); // this will might cause problem because in react it will work asychronically.
+    setListData((listData) => {
+      const updateList = [...listData, task];
+      console.log(updateList);
+      setTask("");
+      return updateList;
+    });
+  }
+
+  function removeTask(i) {
+    const updateListData = listData.filter((elem, id) => {
+      return i != id;
+    });
+    setListData(updateListData);
+  }
+
+  function removeAll() {
+    setListData([]);
+  }
+
   return (
     <>
-      <CustomForm addTask={addTask} />
-      {tasks && <TaskList tasks={tasks} />}
+      <div className="container">
+        <div className="header">TODO LIST</div>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button className="btn" onClick={addTask}>
+          Add
+        </button>
+        <p className="list-heading">Here is you list</p>
+        {listData != [] &&
+          listData.map((data, i) => {
+            return (
+              <>
+                <p key={i}>
+                  <div className="list-data">{data}</div>
+                  <button className="remove" onClick={() => removeTask(i)}>
+                    remove
+                  </button>
+                </p>
+              </>
+            );
+          })}
+        {listData.length >= 1 && (
+          <button onClick={removeAll}>Remove All</button>
+        )}
+      </div>
     </>
   );
 }
--- Projects and Assignments/04- React/01- TODO App/todo-app/src/components/TaskItems.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TaskItems = ({ task }) => {
-  return <div>{task.name}</div>;
-};
-
-export default TaskItems;
