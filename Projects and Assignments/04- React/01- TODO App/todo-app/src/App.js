95d86d0b08c032e154223e945cdd449c44630434
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-22 16:55:55 +0530

    edit button

--- Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
+++ Projects and Assignments/04- React/01- TODO App/todo-app/src/App.js
@@ -1,3 +1,4 @@
+import { isEditable } from "@testing-library/user-event/dist/utils";
 import { useState, useEffect } from "react";
 import "./index.css";
 
@@ -14,9 +15,12 @@ const getLocalListData = () => {
 function App() {
   const [task, setTask] = useState("");
   const [listData, setListData] = useState(getLocalListData());
+  const [isEdited, setIsEdited] = useState(false);
+  const [finalEdit, setFinalEdit] = useState(0);
 
   useEffect(() => {
     localStorage.setItem("todo", JSON.stringify(listData));
+    // console.log(listData);
   }, [listData]);
 
   function addTask() {
@@ -30,6 +34,32 @@ function App() {
         return updateList;
       });
     }
+  }
+
+  function editTask(i) {
+    setTask(listData[i]);
+    setIsEdited((prev) => !prev);
+    setFinalEdit(i);
+  }
+
+  function updateTask() {
+    const tempArr = [...listData];
+    if (task.length > 0) {
+      tempArr[finalEdit] = task;
+      setListData(tempArr);
+    }
+    // if (task.length > 0) {
+    //   setListData((listData) => {
+    //     const updateList = [...listData, task];
+    //     // console.log(updateList);
+    //     setTask("");
+    //     return updateList;
+    //   });
+    // }
+    setTask("");
+    setIsEdited(false);
+    // console.log(finalEdit);
+    // console.log(tempArr);
   }
 
   function removeTask(i) {
@@ -57,12 +87,21 @@ function App() {
             onChange={(e) => setTask(e.target.value)}
             className="flex h-10 rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
           />
-          <button
-            className="active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4"
-            onClick={addTask}
-          >
-            Add
-          </button>
+          {isEdited ? (
+            <button
+              className="active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4"
+              onClick={() => updateTask()}
+            >
+              Update
+            </button>
+          ) : (
+            <button
+              className="active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-indigo-600 focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-indigo-700 dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-indigo-600 text-white hover:bg-indigo-700 h-10 py-2 px-4"
+              onClick={addTask}
+            >
+              Add
+            </button>
+          )}
         </div>
 
         <p className="text-white">Here is you list</p>
@@ -71,14 +110,20 @@ function App() {
             return (
               <>
                 <p key={i} className="flex justify-center items-start gap-2">
-                  <div className="w-[42rem] text-left text-white bg-[#e3e3e3] inline-flex items-center rounded-md text-sm font-medium transition-colors bg-[#515151] hover:bg-[#434343] hover:cursor-default py-2 px-4 min-h-[2.5rem]">
+                  <div className="w-[32rem] text-left text-white inline-flex items-center rounded-md text-sm font-medium transition-colors bg-[#515151] hover:bg-[#434343] hover:cursor-default py-2 px-4 min-h-[2.5rem]">
                     {data}
                   </div>
                   <button
-                    className="text-white active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-1 bg-[#e44f54] focus:ring-indigo-400 focus:ring-offset-1 dark:hover:bg-[#d03237] dark:hover:text-gray-100 disabled:opacity-50 dark:focus:ring-indigo-400 disabled:pointer-events-none dark:focus:ring-offset-gray-900 dark:bg-[#e44f54] text-white hover:bg-[#d03237] h-10 py-2 px-4"
+                    className="text-white active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none bg-[#1cae02] dark:hover:text-gray-100 text-white hover:bg-[#178407] h-10 py-2 px-4 "
+                    onClick={() => editTask(i)}
+                  >
+                    edit
+                  </button>
+                  <button
+                    className="text-white active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors bg-[#e44f54] hover:bg-[#d03237] h-10 py-2 px-4"
                     onClick={() => removeTask(i)}
                   >
-                    Remove
+                    remove
                   </button>
                 </p>
               </>
