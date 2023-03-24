985633e4d834442ab31c3e51e7033e14097ead43
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-24 21:59:20 +0530

    router

--- Code Practice/WEEK18- DAY02 - 05th MAR- SUNDAY/router/src/App.js
+++ Code Practice/WEEK18- DAY02 - 05th MAR- SUNDAY/router/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { About } from "./pages/About";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
