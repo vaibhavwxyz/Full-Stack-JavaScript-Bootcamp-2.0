45c4d50337195791f94c8b4fd8077db5509b5aa0
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-28 14:46:51 +0530

    ui done

--- Projects and Assignments/04- React/02- Rock Paper Scissor/rock-paper-scissor/src/App.js
+++ Projects and Assignments/04- React/02- Rock Paper Scissor/rock-paper-scissor/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./index.css";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function App() {
   const choice = ["rock", "paper", "scissor"];
@@ -10,9 +12,11 @@ function App() {
   const [computerScore, setComputerScore] = useState(0);
   const [result, setResult] = useState("Play");
 
+  const reset = () => window.location.reload();
+
   return (
     <>
-      <div className="p-5 space-y-5 text-white">
+      <div className="p-5 space-y-12 text-white">
         {/* tittle */}
         <div>
           <h1 className="mx-auto text-center text-2xl mb-2">
@@ -31,24 +35,62 @@ function App() {
         </div>
         {/* result */}
         <div className="mx-auto text-center">
-          <h3>You Win!!</h3>
-          <button>Reset</button>
+          <h3 className="mb-2">You Win!!</h3>
+          <Button variant="outlined" onClick={reset}>
+            Reset
+          </Button>
         </div>
         {/* choice */}
-        <div>
+        <div className="flex justify-between px-10 items-center">
           {/* your choice */}
-          <div>
-            <img src="" alt="" />
+          <div className="flex flex-col items-center">
+            <h3>Your choice</h3>
+            <div className="rounded-full w-fit p-5 hover:bg-[#374151] bg-[#111827]">
+              <img
+                src="../assets/paper.png"
+                alt="user-choice"
+                className="w-10 sm:w-16"
+              />
+            </div>
           </div>
           {/* computer choice */}
-          <div>
-            <img src="" alt="" />
+          <div className="flex flex-col items-center">
+            <h3>computer Choice</h3>
+            <div className="rounded-full w-fit p-5 hover:bg-[#374151] bg-[#111827]">
+              <img
+                src="../assets/paper.png"
+                alt="computer-choice"
+                className="w-10 sm:w-16"
+              />
+            </div>
           </div>
         </div>
         {/* lock your choice */}
-        <div>
-          <h3 className="text-center">Lock Your Choice</h3>
-          <img src={`../`} alt="" />
+        <div className="flex flex-col justify-center items-center">
+          <h3 className="text-center mb-2">Lock Your Choice</h3>
+          <div className="space-x-2 ">
+            <button className="hover:bg-[#374151] bg-[#111827] sm:active:scale-95 rounded-full p-5">
+              <img
+                src="../assets/rock.png"
+                alt="rock"
+                className="w-10 sm:w-16"
+              />
+            </button>
+            <button className="hover:bg-[#374151] bg-[#111827] sm:active:scale-95 rounded-full p-5">
+              <img
+                src="../assets/paper.png"
+                alt="paper"
+                className="w-10 sm:w-16"
+              />
+            </button>
+          </div>
+          <button className="hover:bg-[#374151] bg-[#111827] sm:active:scale-95 rounded-full p-5">
+            <img
+              src="../assets/scissor.png"
+              alt="paper"
+              className="w-10 sm:w-16"
+            />
+          </button>
         </div>
       </div>
     </>
