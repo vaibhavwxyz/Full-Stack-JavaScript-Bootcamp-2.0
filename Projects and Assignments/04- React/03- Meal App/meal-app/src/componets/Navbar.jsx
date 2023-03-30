fdd0c6b2df55c81f5df83336f91c73d854bc3dfd
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-30 12:11:51 +0530

    navbar

---
+++ Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/Navbar.jsx
@@ -0,0 +1,10 @@
+function Nav() {
+  return (
+    <nav className="nav-container">
+      <div className="logo-wrapper"></div>
+      <div className="btn-wrapper">
+        <button className="login"></button>
+      </div>
+    </nav>
+  );
+}
