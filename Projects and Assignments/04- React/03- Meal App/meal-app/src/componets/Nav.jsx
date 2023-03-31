05eed5b0bbb12feb6ece9227a1ad52dc119718ce
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-03-31 21:34:10 +0530

    meal-app

--- Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/Nav.jsx
+++ Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/Nav.jsx
@@ -1,9 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+
 function Nav() {
   return (
     <nav className="nav-container">
       <div className="logo-wrapper"></div>
       <div className="btn-wrapper">
-        <button className="login">login</button>
+        <button className="login">
+          <FontAwesomeIcon icon={faUtensils} /> login
+        </button>
       </div>
     </nav>
   );
