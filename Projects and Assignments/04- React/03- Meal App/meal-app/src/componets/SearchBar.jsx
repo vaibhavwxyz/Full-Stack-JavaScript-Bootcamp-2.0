127ee3175e9c00fbfdd0db4e7733f148dc02f5f7
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-04-06 01:09:51 +0530

    final commit

--- Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/SearchBar.jsx
+++ Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/SearchBar.jsx
@@ -7,35 +7,44 @@ let URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const SearchBar = ({ recipes, setRecipes }) => {
   const [search, setSearch] = useState("");
 
-  const searchRecpie = async () => {
-    if (search.length > 0) {
-      const result = await axios.get(URL + search);
-      const data = await result.data;
-      console.log(data);
-      setRecipes(data.meals);
-    }
-
-    if (recipes.length <= 20 && search == "") {
-      const data = fetchMealItems().then((data) => {
-        // console.log(data);
+  const searchRecpie = async (e) => {
+    e.preventDefault();
+    try {
+      if (search.length > 0) {
+        const result = await axios.get(URL + search);
+        const data = await result.data;
+        console.log(data);
         setRecipes(data.meals);
-      });
+      }
+
+      if (recipes.length <= 20 && search == "") {
+        const data = fetchMealItems().then((data) => {
+          // console.log(data);
+          setRecipes(data.meals);
+        });
+      }
+    } catch (er) {
+      // alert("recepies not found");
+      console.log(er);
     }
   };
 
   return (
     // <form >
-    <div className="mb-5 bg-[#fee5e5] text-[#ef4f5f] placeholder:text-[#ef4f5f] rounded-full max-w-2xl flex justify-between items-center py-2 px-4">
+    <form
+      className="mb-5 bg-[#fee5e5] text-[#ef4f5f] placeholder:text-[#ef4f5f] rounded-full max-w-2xl flex justify-between items-center py-2 px-4"
+      onSubmit={searchRecpie}
+    >
       <input
         type="text"
         placeholder="Search any recipe"
         className="bg-[#fee5e5] text-[#ef4f5f] placeholder:text-[#ef4f5f] px-1 rounded-full outline-none text-lg w-full"
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button onClick={searchRecpie}>
+      <button type="submit">
         <FontAwesomeIcon icon={faSearch} />
       </button>
-    </div>
+    </form>
   );
 };
 
--- Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/Section.jsx
+++ Projects and Assignments/04- React/03- Meal App/meal-app/src/componets/Section.jsx
@@ -4,9 +4,10 @@ import MealCard from "./MealCard";
 const Section = ({ recipes }) => {
   return (
     <div className="grid grid-cols-3 gap-5 ">
-      {recipes.map((recipe, idx) => {
-        return <MealCard key={idx} recipe={recipe} />;
-      })}
+      {recipes &&
+        recipes.map((recipe, idx) => {
+          return <MealCard key={idx} recipe={recipe} />;
+        })}
     </div>
   );
 };
