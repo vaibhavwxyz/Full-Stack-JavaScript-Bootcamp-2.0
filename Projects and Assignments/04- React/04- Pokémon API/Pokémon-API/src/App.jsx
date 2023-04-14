3d6a6e4eb82e4318a45684161621f3bfba703254
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-04-14 17:52:54 +0530

    pokemon api

--- Projects and Assignments/04- React/04- Pokémon API/Pokémon-API/src/App.jsx
+++ Projects and Assignments/04- React/04- Pokémon API/Pokémon-API/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemon, setPokemon] = useState({});
+
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => {
+        console.log(response);
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+        });
+        console.log(pokemon);
+      });
+  };
 
   return (
-    
-  )
+    <div>
+      <h1>Pokémon Stats</h1>
+      <input type="text" onChange={(e) => setPokemonName(e.target.value)} />
+      <button onClick={searchPokemon}>Search</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
