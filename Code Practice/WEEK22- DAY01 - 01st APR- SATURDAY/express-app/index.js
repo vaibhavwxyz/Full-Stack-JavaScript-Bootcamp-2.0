6b762a2f25cd8ecb69de5c2fef726ed992cf4cc4
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-06-07 22:34:32 +0530

    express

---
+++ Code Practice/WEEK22- DAY01 - 01st APR- SATURDAY/express-app/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/insta", (req, res) => {
+  // res.send("<h1>Hello World!</h1>");
+  const insta = {
+    username: "vaibhavwxzy",
+    follower: 18,
+    following: 10,
+  };
+  res.status(200).json({ insta });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
