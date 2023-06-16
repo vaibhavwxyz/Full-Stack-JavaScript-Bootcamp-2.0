966c1824e8a4995d22d9b805112ffe489083da59
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-06-16 22:59:21 +0530

    express

--- Code Practice/WEEK22- DAY02 - 02nd APR- SUNDAY/NodeJS/index.js
+++ Code Practice/WEEK22- DAY02 - 02nd APR- SUNDAY/NodeJS/index.js
@@ -4,6 +4,29 @@
 
 // const http = require("http")
 const fs = require("fs");
+const mongoose = require("mongoose")
+
+// Schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  gender: {
+    type: String
+  },
+  jobTitle: {
+    type: String
+  }
+})
 
 // const myServer = http.createServer((req, res) => {
 //   const log = `${new Date().getDate().toLocaleString()}: ${req.url} New Req Received\n`;
