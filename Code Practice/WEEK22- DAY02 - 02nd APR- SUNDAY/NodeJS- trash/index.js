e59524576ae020ba9606acd83adddfa9a915cd15
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-06-24 01:59:05 +0530

    nodejs

--- Code Practice/WEEK22- DAY02 - 02nd APR- SUNDAY/NodeJS- trash/index.js
+++ Code Practice/WEEK22- DAY02 - 02nd APR- SUNDAY/NodeJS- trash/index.js
@@ -16,10 +16,28 @@ fs.copyFile("text.txt", "log.txt", callback);
 //! http
 
 const http = require("http");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  console.log("New Req Rec");
-  res.end("Hello from Server");
+  const log = `${Date.now()}: ${req.url} New Req Received\n`;
+  const myUrl = url.parse(req.url, true);
+
+  fs.appendFile("log.txt", log, () => {
+    switch (req.url) {
+      case "/":
+        console.log(myUrl);
+        res.end("Homepage");
+        break;
+      case "/about":
+        res.end("About");
+        break;
+      case "/contact":
+        res.end("Contact");
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  });
 });
 
 myServer.listen(8000, () => console.log("Server Started"));
