872df350b7292a6da364b7f2613dcda8b43fa4fc
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-06-27 00:44:46 +0530

    mongoose

--- Code Practice/WEEK22- DAY02 - 02nd APR- SUNDAY/NodeJS- might delete later/app.js
+++ Code Practice/WEEK22- DAY02 - 02nd APR- SUNDAY/NodeJS- might delete later/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const url = require("url");
 const users = require("./MOCK_DATA.json");
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 8000;
 
 // const myServer = http.createServer((req, res) => {
@@ -29,6 +30,35 @@ const PORT = 8000;
 
 // myServer.listen(PORT, () => console.log(`Server Started at ${PORT}`));
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/first_mongoose")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Mongo error: ", err));
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  jobTitle: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -51,16 +81,27 @@ app.get("/api/users", (req, res) => {
 app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
   const filteredUser = users.find((user) => user.id === id);
+  if (!filteredUser)
+    return res.status(404).json({ error: "User Not Found 404" });
   return res.json(filteredUser);
 });
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.json({ msg: "All Field Required" });
+  }
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Success", id: users.length + 1 });
+    return res.json({ status: "Success", id: users.length });
   });
-  return res.json();
 });
 
 app.listen(PORT, () => console.log(`Server is Running at ${PORT}`));
